3bbba5b2ef1e9ce04d497258dbd9a1a4cfcc800d
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-10-02 19:18:28 +0330

    trying to set theme

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import ThemeToggle from "@/components/themeToggle";
 
 export default function Home() {
   return (
@@ -96,6 +97,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <ThemeToggle />
     </div>
   );
 }
