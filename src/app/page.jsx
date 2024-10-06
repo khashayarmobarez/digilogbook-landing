d25b9d6bbcc634f67d7603c1766225edd62573cd
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-10-06 14:30:49 +0330

    Refactor page and add Introduction component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,24 @@
 'use client';
-
-import { useContext } from 'react';
-import { ThemeContext } from '@/utils/ThemeContext';
 import Image from "next/image";
+import { ThemeContext } from '@/utils/ThemeContext';
+import { useContext } from 'react';
+
+// comps
+import Introduction from '@/components/Landing/Introduction';
 
 export default function Home() {
 
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <div className="">
-      <main className=" text-2xl font-bold">
-        سامانه ثبت اطلاعات و پروازهای روزانه
-      </main>
+    <main className="">
+      
+      <Introduction />
       
       {/* <div>
         <p>Current theme: {theme}</p>
         <button onClick={toggleTheme}>Toggle Theme</button>
       </div> */}
-    </div>
+    </main>
   );
 }
---
+++ src/components/Landing/Introduction.jsx
@@ -0,0 +1,16 @@
+
+
+const Introduction = () => {
+    return (
+        <div className={'w-full h-[90dvh] IntroductionBackground'}>
+        </div>
+        // <style jsx>{`
+        //     .IntroductionBackground {
+        //         background-image: url('/Assets/images/landing/introduction-background.jpg');
+        //         background-size: cover;
+        //         background-position: center;
+        // `}</style>
+    );
+};
+
+export default Introduction;
