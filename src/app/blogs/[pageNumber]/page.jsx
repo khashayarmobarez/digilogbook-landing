f14542915afb42ec344f9add301a116b40dea452
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-10-28 16:58:39 +0330

    adding blogs

---
+++ src/app/blogs/[pageNumber]/page.jsx
@@ -0,0 +1,104 @@
+
+import { Api_BASE_URL } from '@/utils/resuableVars';
+import Image from 'next/image';
+import Link from 'next/link';
+
+// mui
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+
+export async function generateStaticParams() {
+  const pageSize = 4;
+  const totalPages = await getTotalPages(pageSize);
+  
+  return Array.from({ length: totalPages }, (_, i) => ({
+    pageNumber: (i + 1).toString(),
+  }));
+}
+
+async function getTotalPages(pageSize) {
+  const res = await fetch(`${Api_BASE_URL}/Blog/GetBlogs?pageSize=${pageSize}&pageNumber=1`);
+  const data = await res.json();
+  return data.totalPagesCount;
+}
+
+export default async function BlogsPage({ params }) {
+  const pageNumber = parseInt(params.pageNumber) || 1;
+  const pageSize = 4;
+
+  const res = await fetch(
+    `${Api_BASE_URL}/Blog/GetBlogs?pageSize=${pageSize}&pageNumber=${pageNumber}`,
+    { next: { revalidate: 3600 } }
+  );
+  const { data: blogs, totalPagesCount } = await res.json();
+
+
+  return (
+    <div className='w-full flex justify-center'>
+        <div className='w-[90%] flex flex-col gap-y-10 my-10 md:w-[60%]'>
+
+            <h1 className='text-lg text-accentNormal md:text-3xl font-semibold'>بلاگ و مقالات</h1>
+
+            <ul className='w-full grid grid-cols-1 gap-4 md:grid-cols-2 '>
+                {blogs.map((blog) => (
+                <li key={blog.id} className=' border border-1 border-neutralDark flex flex-col items-center justify-center p-4 rounded-2xl'>
+                    <Link href={`/blogs/${blog.id}`} className='w-full'>
+                        <div className='w-full text-start flex flex-col items-start gap-y-1'>
+                            <Image src={blog.image.path} alt={blog.image.name} 
+                            width={100} // Set a base width; will be overridden by CSS
+                            height={80} // Fixed height
+                            className='w-full h-56'  />
+
+                            <h2 className='text-xl font-semibold text-accentNormal'>{blog.title}</h2>
+
+                            <p>{blog.authorName}</p>
+
+                            <p className=' text-right text-base mt-2'>{blog.blogSections[0].htmlContent.slice(0,110)} ...</p>
+
+                            <div className='w-full flex flex-col gap-y-4 md:flex-row items-center mt-2 md:justify-between'>
+
+                                <div className='w-full flex justify-between md:w-[61%]'>
+
+                                    <div className='flex items-center justify-center gap-x-2'>
+                                        <AccessTimeOutlinedIcon fontSize='8px' />
+                                        <p className='text-xs'>در {blog.timeToReadInMinutes} دقیقه بخوانید</p>
+                                    </div>
+
+                                    <div className='flex items-center justify-center gap-x-2'>
+                                        <CalendarTodayOutlinedIcon fontSize='8px' />
+                                        <p className='text-xs'>تاریخ: {blog.createDate}</p>
+                                    </div>
+
+                                </div>
+
+                                <Link href={`/blog/${blog.id}`} className=' gap-x-2 rounded-3xl link link-hover flex text-accentNormal'>
+                                    <ArrowForwardIcon />
+                                    <p>خواندن مقاله</p>
+                                </Link>
+
+                            </div>
+                        </div>
+                    </Link>
+                </li>
+                ))}
+            </ul>
+
+            <Pagination pageNumber={pageNumber} totalPages={totalPagesCount} />
+
+        </div>
+    </div>
+  );
+}
+
+function Pagination({ pageNumber, totalPages }) {
+  return (
+    <div className="pagination">
+      {Array.from({ length: totalPages }, (_, i) => (
+        <Link key={i} href={`/blogs/${i + 1}`} prefetch={false}>
+          <p className={i + 1 === pageNumber ? 'active' : ''}>{i + 1}</p>
+        </Link>
+      ))}
+    </div>
+  );
+}
