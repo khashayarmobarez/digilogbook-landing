2de2a7a149c7462bcf274edd580e076ce5f60c13
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-11-10 12:48:37 +0330

    refactor: extract blog listing into BlogsBlogBoxes component for improved code organization

--- src/app/blogs/[pageNumber]/page.jsx
+++ src/app/blogs/[pageNumber]/page.jsx
@@ -1,11 +1,9 @@
 import { Api_BASE_URL } from '@/utils/resuableVars';
-import Image from 'next/image';
-import Link from 'next/link';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
-import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
-import Pagination from '@/components/module/blogs/Pagination';
 import { fakeApi } from '@/utils/FakeApiBackup';
+
+// comps
+import BlogsBlogBoxes from '@/components/module/blogs/BlogsBlogBoxes';
+import Pagination from '@/components/module/blogs/Pagination';
 
 export default async function BlogsPage({ params }) {
   const pageNumber = parseInt(params.pageNumber) || 1;
@@ -35,37 +33,7 @@ export default async function BlogsPage({ params }) {
 
         <h1 className='text-lg text-accentNormal md:text-3xl font-semibold'>بلاگ و مقالات</h1>
 
-        <ul className='w-full grid grid-cols-1 gap-4 md:grid-cols-2'>
-          {blogs.map((blog) => (
-            <li key={blog.id} className='border border-1 border-neutralDark flex flex-col items-center justify-center p-4 rounded-[24px]'>
-              <Link href={`/blogs/blog/${blog.id}`} className='w-full'>
-                <div className='w-full text-start flex flex-col items-start gap-y-1 text-neutralDark'>
-                  <Image src={blog.image.path} alt={'image'} width={100} height={80} className='w-full h-56 rounded-[24px]' />
-                  <h2 className='text-xl font-semibold text-accentNormal mt-4'>{blog.title}</h2>
-                  <p className='md:text-lg'>{blog.authorName}</p>
-                  <p className='text-right text-base my-3'>{blog.blogSections[0].htmlContent.slice(0, 130)} ...</p>
-
-                  <div className='w-full flex flex-col gap-y-6 xl:flex-row items-center mt-2 xl:justify-between'>
-                    <div className='w-full flex justify-between xl:w-[61%]'>
-                      <div className='flex items-center justify-center gap-x-2'>
-                        <AccessTimeOutlinedIcon fontSize='8px' />
-                        <p className='text-xs'>در {blog.timeToReadInMinutes} دقیقه بخوانید</p>
-                      </div>
-                      <div className='flex items-center justify-center gap-x-2'>
-                        <CalendarTodayOutlinedIcon fontSize='8px' />
-                        <p className='text-xs'>تاریخ: {blog.createDate}</p>
-                      </div>
-                    </div>
-                    <div className='gap-x-2 rounded-3xl link link-hover flex text-lg text-accentNormal items-center'>
-                      <ArrowForwardIcon />
-                      <p>خواندن مقاله</p>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <BlogsBlogBoxes blogsData={blogs} />
 
         <Pagination pageNumber={pageNumber} totalPages={totalPagesCount} />
 
---
+++ src/components/module/blogs/BlogsBlogBoxes.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+
+const BlogsBlogBoxes = ({blogsData}) => {
+    return (
+        <ul className='w-full grid grid-cols-1 gap-4 md:grid-cols-2'>
+          {blogsData.map((blog) => (
+            <li key={blog.id} className='border border-1 border-neutralDark flex flex-col items-center justify-center p-4 rounded-[24px]'>
+              <Link href={`/blogs/blog/${blog.id}`} className='w-full'>
+                <div className='w-full text-start flex flex-col items-start gap-y-1 text-neutralDark'>
+                  <Image src={blog.image.path} alt={'image'} width={100} height={80} className='w-full h-56 rounded-[24px]' />
+                  <h2 className='text-xl font-semibold text-accentNormal mt-4'>{blog.title}</h2>
+                  <p className='md:text-lg'>{blog.authorName}</p>
+                  <p className='text-right text-base my-3'>{blog.blogSections[0].htmlContent.slice(0, 130)} ...</p>
+
+                  <div className='w-full flex flex-col gap-y-6 xl:flex-row items-center mt-2 xl:justify-between'>
+                    <div className='w-full flex justify-between xl:w-[61%]'>
+                      <div className='flex items-center justify-center gap-x-2'>
+                        <AccessTimeOutlinedIcon fontSize='8px' />
+                        <p className='text-xs'>در {blog.timeToReadInMinutes} دقیقه بخوانید</p>
+                      </div>
+                      <div className='flex items-center justify-center gap-x-2'>
+                        <CalendarTodayOutlinedIcon fontSize='8px' />
+                        <p className='text-xs'>تاریخ: {blog.createDate}</p>
+                      </div>
+                    </div>
+                    <div className='gap-x-2 rounded-3xl link link-hover flex text-lg text-accentNormal items-center'>
+                      <ArrowForwardIcon />
+                      <p>خواندن مقاله</p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+    );
+};
+
+export default BlogsBlogBoxes;
