a5567697e3e8989bbdf4cac9202a3e70b95a543a
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-11-18 16:23:08 +0330

    style: update blog links and adjust spacing in LandingBlogBox and BlogPage components for improved layout

--- src/components/templates/BlogPage.jsx
+++ src/components/templates/BlogPage.jsx
@@ -8,15 +8,15 @@ import CommentSection from '@/components/module/blogs/CommentSection';
 
 const BlogPage = ({blogData}) => {
     return (
-        <div className='w-[90%] flex flex-col pt-12 md:w-[60%]'>
+        <div className='w-[90%] flex flex-col mt-10 md:pt-[60px] md:w-[60%]'>
 
             <h1 className=' text-xl md:text-2xl font-semibold text-accentNormal'>{blogData.title}</h1>
 
-            <p className=' text-base md:text-lg mt-2 '>{blogData.authorName}</p>
+            <p className=' text-base md:text-lg mt-2 md:mt-4 '>{blogData.authorName}</p>
 
-            <Image src={blogData.image.path} alt={'image'} width={100} height={80} className='w-full h-40 lg:h-80 rounded-3xl border mt-4 object-cover' />
+            <Image src={blogData.image.path} alt={'image'} width={100} height={80} className='w-full h-40 lg:h-80 rounded-3xl border mt-5 md:mt-8 object-cover' />
 
-            <div className='w-full grid grid-cols-2 md:grid-cols-4 gap-4 text-xs my-6'>
+            <div className='w-full grid grid-cols-2 md:grid-cols-4 gap-4 text-xs my-4'>
 
                     <div className='flex justify-start items-center gap-x-2'>
                         <span className='w-4 h-4'>
@@ -55,7 +55,7 @@ const BlogPage = ({blogData}) => {
                             <h1>{section.title}</h1>
                             {
                                 section.image &&
-                                    <Image src={section.image.path} alt={section.image.name} width={100} height={100} className='w-full h-40 rounded-3xl object-cover mt-2' />
+                                    <Image src={section.image.path} alt={section.image.name} width={100} height={100} className='w-full h-40 rounded-3xl object-cover mt-1 md:mt-2' />
                             }
                             <p className=''>{section.htmlContent}</p>
                             <br/>
