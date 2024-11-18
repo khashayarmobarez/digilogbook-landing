12a05e0fedafc626a5d2e151632db1003f7a8d98
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-11-18 13:18:05 +0330

    style: adjust spacing and font size in AboutUs component for improved readability

--- src/app/aboutUs/page.jsx
+++ src/app/aboutUs/page.jsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 const AboutUs = () => {
     return (
         <div className="w-full flex justify-center">
-            <div className="w-[90%] flex flex-col items-start justify-between  gap-y-8 my-10 md:min-h-[40dvh] md:w-[60%] md:gap-y-[30px] md:my-[60px]">
+            <div className="w-[90%] flex flex-col items-start justify-between gap-y-5 my-10 md:min-h-[40dvh] md:w-[60%] md:gap-y-[30px] md:my-[60px]">
 
-                <h1 className=" font-bold text-base md:text-3xl text-accentNormal">درباره ما</h1>
+                <h1 className=" font-bold text-lg md:text-3xl text-accentNormal">درباره ما</h1>
 
                 <p className="text-start ">
                     یک لاگ بوک دیجیتال طراحی کرده ایم تا شما بتوانید به راحتی با ثبت پروازهای خود طبق سیلابس های آموزشی به آموزشتان نظم بیشتری دهید و دسترسی سریع به سوابق خود برای آگاهی از پیشرفتتان داشته باشید. علاوه بر این ، به شما امکان این را می دهد که بدانید از چه مرحله ایی آغاز کرده اید ، تا بتوانید برای خود دقیق تر و راحت تر هدف گذاری کنید ، همچنین نقاط قوت و ضعف خود را یادداشت کرده و در راستای بهبود آن ها تلاش بیشتری کنید.
