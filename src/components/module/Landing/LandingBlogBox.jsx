a47f8618f1b7bf4f19a229a19499fe60a483a24a
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-11-20 18:14:20 +0330

    fix: adjust gap in LandingBlogBox for improved layout and spacing

--- src/components/module/Landing/LandingBlogBox.jsx
+++ src/components/module/Landing/LandingBlogBox.jsx
@@ -12,13 +12,13 @@ const LandingBlogBox = ({blog}) => {
     const { title, authorName, image, createDate, id, blogSections, timeToReadInMinutes, blogVisitCount} = blog;
     return (
         <article className='w-full h-full bg-primaryNormal rounded-3xl flex flex-col items-center justify-center gap-y-8 p-4 md:p-8 text-primaryLight
-        md:flex-row md:justify-between md:w-[100%] md:h-full md:mt-0'
+        md:flex-row md:justify-between md:w-[100%] md:h-full md:mt-0 md:gap-x-8'
         style={{boxShadow:'inset 0 0 2px 1px rgba(255, 255, 255, 0.2)'}}>
             <div className="w-full h-[14rem]
             md:h-full md:px-0 md:w-[80%]">
                 <img src={image?.path} alt={image?.name || 'picture'} className="w-full h-full rounded-3xl" />
             </div>
-            <div className='w-full h-auto flex flex-col items-center justify-between md:pr-4  md:h-full '>
+            <div className='w-full h-auto flex flex-col items-center justify-between  md:h-full '>
 
                 <div className='w-full flex flex-col items-start text-start justify-center gap-y-4 md:gap-y-0 text-neutralLightActive'>
                     <h1 className=" font-bold text-start">{title}</h1>
