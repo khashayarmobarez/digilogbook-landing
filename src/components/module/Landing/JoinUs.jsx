ea4ef27591ee82a10bb0eeb3c4bda358d17963d6
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-12-26 16:06:26 +0330

    landing

--- src/components/module/Landing/JoinUs.jsx
+++ src/components/module/Landing/JoinUs.jsx
@@ -2,12 +2,12 @@
 
 const JoinUs = () => {
     return (
-        <div className="w-full flex justify-center mt-20 mb-10">
-            <div className="w-[90%] flex flex-col justify-between items-center rounded-3xl py-4 gap-y-4 md:px-16 md:py-9 bg-primaryLightHover md:flex-row">
+        <div className="w-full flex justify-center mt-10 mb-4">
+            <div className="w-[90%] flex flex-col justify-between items-center rounded-3xl p-4 gap-y-4 md:px-16 md:py-9 bg-primaryLightHover md:flex-row">
 
                 <p className=" font-bold text-base md:text-3xl text-neutralDark">برای مشاهده قابلیت‌های بیشتر همراه ما باشید...</p>
 
-                <div className="w-full flex flex-row justify-around items-center md:gap-x-8 md:w-[30%] ">
+                <div className="w-full flex flex-row justify-between items-center md:gap-x-8 md:w-[30%] ">
                     <button className="btn btn-outline w-[44%] text-accentNormal rounded-3xl
                     md:w-[45%]"
                     onClick={() => window.open('https://webapp.digilogbook.app/login', '_blank')}>
