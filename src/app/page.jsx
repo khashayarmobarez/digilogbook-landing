8cbf5e5f1b1ca52a38f01b56233dd0badca94aa7
khashayar mobarez <khashayarmobarez333@gmail.com>
2025-02-11 13:38:42 +0330

    refactoring 2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,57 +1,9 @@
+import Landing from "@/components/templates/Landing";
 
-import Image from "next/image";
-import { ThemeContext } from '@/utils/ThemeContext';
-import { Api_BASE_URL } from "@/utils/resuableVars";
-// import { useContext } from 'react';
-
-// comps
-import Introduction from '@/components/module/Landing/Introduction';
-import WhyDigi from "@/components/module/Landing/WhyDigi";
-import Options from "@/components/module/Landing/Options";
-import ArticleSlider from "@/components/module/Landing/ArticleSlider";
-import BrandsMarquee from "@/components/module/Landing/BrandsMarquee";
-
-// react query
-import ContactUsBox from "@/components/module/Landing/ContactUsBox";
-import JoinUs from "@/components/module/Landing/JoinUs";
-
-async function getBlogs() {
-  
-  const pageSize = 5;
-  const pageNumber = 1;
-  
-  try {
-      const res = await fetch(`${Api_BASE_URL}/Blog/GetBlogs?pageSize=${pageSize}&pageNumber=${pageNumber}`, {
-          next: { revalidate: 3600 }, // Revalidate every hour (for ISR)
-      });
-      if (!res.ok) throw new Error('Failed to fetch data');
-      return await res.json();
-  } catch (error) {
-      console.error('Error fetching data:', error);
-      return null; // Fallback in case of an error
-  }
-}
-
-export default async function Home() {
-
-  // const { theme, toggleTheme } = useContext(ThemeContext);
-  const blogsData = await getBlogs();
+export default async function Index() {
 
   return (
-    <>
-      <main className="pb-10">
-          <Introduction />
-          <WhyDigi />
-          <Options />
-          <JoinUs />
-          <ArticleSlider blogsData={blogsData} />
-          {/* <BrandsMarquee /> */}
-          <ContactUsBox />
-          {/* <div>
-            <p>Current theme: {theme}</p>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-            </div> */}
-      </main>
-    </>
+    <Landing />
   );
+  
 }
